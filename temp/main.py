a0cad3360bbda0f376f3370151d28df623f510dd
兮尘 <761516186@qq.com>
2022-03-30 16:19:17 +0800

    Update main.py

--- temp/main.py
+++ temp/main.py
@@ -1,11 +1,11 @@
 import argparse
 
 from load_data import Data
-from toolbox.nn.TuckERCPD import TuckERCPD
-from toolbox.nn.TuckERT import TuckERT
-from toolbox.nn.TuckERTNT import TuckERTNT
-from toolbox.nn.TuckERTTR import TuckERTTR
-from toolbox.nn.TuckERTTT import TuckERTTT
+from ..toolbox.nn.TuckERCPD import TuckERCPD
+from ..toolbox.nn.TuckERT import TuckERT
+from ..toolbox.nn.TuckERTNT import TuckERTNT
+from ..toolbox.nn.TuckERTTR import TuckERTTR
+from ..toolbox.nn.TuckERTTT import TuckERTTT
 from train import train_temporal
 
 
