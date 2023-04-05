1eb86b0f98a7eaa49256fb6305eae0280d0b444c
兮尘 <23211526+LinXueyuanStdio@users.noreply.github.com>
2023-04-05 18:25:29 +0800

    Update sampling_CQE.py

--- sampling_CQE.py
+++ sampling_CQE.py
@@ -4,18 +4,10 @@
 """
 from ComplexTemporalQueryData import *
 
-dataset = ICEWS14()
+# dataset = ICEWS14()
+# dataset = ICEWS05_15()
+dataset = GDELT()
 cache_path = ComplexTemporalQueryDatasetCachePath(dataset.cache_path)
 data = ComplexQueryData(dataset, cache_path)
 data.preprocess_data_if_needed()
 data.patch()
-# dataset = ICEWS05_15()
-# cache_path = ComplexTemporalQueryDatasetCachePath(dataset.cache_path)
-# data = ComplexQueryData(dataset, cache_path)
-# data.preprocess_data_if_needed()
-# data.patch()
-# dataset = GDELT()
-# cache_path = ComplexTemporalQueryDatasetCachePath(dataset.cache_path)
-# data = ComplexQueryData(dataset, cache_path)
-# data.preprocess_data_if_needed()
-# data.patch()
